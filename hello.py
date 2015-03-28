80c899f13e009ccc608423bdef5b82dd0e0d335d
david eriksson <davideriksson@swedenmail.com>
2015-03-28 10:33:32 +0100

    Added functionality to flask

--- hello.py
+++ hello.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
+#To turn off debugging
+app.debug = False
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route('/user/<user>')
+def show_user(user):
+    return 'User %s' % user
+
+@app.route('/post_id/<int:post_id>')
+def show_post(post_id):
+    return 'Post %d' % post_id
+
+@app.route('/post', methods=['POST'])
+def post():
+    return 'POST'
+
+with app.test_request_context():
+    print url_for('hello')
+
 if __name__ == "__main__":
-    app.run()
+    #Enable listen to all public IP
+    app.run(host='0.0.0.0')
+    #app.run()

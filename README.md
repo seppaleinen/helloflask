1dff2e0dc3ec2c55eebf449ad4f660939c706c02
david eriksson <davideriksson@swedenmail.com>
2015-03-29 16:00:58 +0200

    travis

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # flask
 
 pip install Flask
+pip install coverage
+pip install coveralls
 python hello.py
  * Running on http://localhost:5000/
--- runserver.py
+++ runserver.py
@@ -1,4 +1,4 @@
 from application import app
-app.run(debug=True)
+app.run(debug=True, host='0.0.0.0')
 #Enable listen to all public IP
 #app.run(host='0.0.0.0')
